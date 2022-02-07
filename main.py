a48e58f0dc527386f1237cbecacd70cfdcc93efd
AdityaKane2001 <adityakane1@gmail.com>
2022-02-07 07:05:40 +0000

    Corrected code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import *
 import pandas as pd
 # Implementing roberta multi input multi task
 
-model_obj = RoBERTa_multi_input_multi_task_plus()
+model_obj = RoBERTa_multi_input_multi_task()
 
 df = pd.read_csv("./dataset/train/train-empathy-distress-prediction-task-gold-text-normalization-Empath-normalize-custom-percent.csv")
 inputs = model_obj.prepare_input(Preprocess(), Utils(), df)
--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative3
 gzip -d GoogleNews-vectors-negative300.bin.gz
 mv GoogleNews-vectors-negative300.bin ../GoogleNews-vectors-negative300.bin
 
+
 pip install --upgrade simpletransformers
 pip install --upgrade autocorrect
+pip install git+https://github.com/MCFreddie777/language-check.git
+pip install pycontractions
