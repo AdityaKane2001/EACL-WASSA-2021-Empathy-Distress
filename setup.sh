f8efca78d339fed3b69a4522398a6204e807c902
AdityaKane2001 <adityakane1@gmail.com>
2022-02-07 06:55:51 +0000

    Corrected code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #Download and save word vectors
 wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
 gzip -d GoogleNews-vectors-negative300.bin.gz
+mv GoogleNews-vectors-negative300.bin ../GoogleNews-vectors-negative300.bin
 
 pip install --upgrade simpletransformers
 pip install --upgrade autocorrect
